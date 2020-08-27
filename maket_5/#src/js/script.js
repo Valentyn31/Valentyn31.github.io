bdc245ae05417cec5fc1027d9a365701ca8e50f7
Valentyn31 <polishchykvalentyn2017@gmail.com>
2020-08-27 08:51:41 +0300

    created header

--- maket_5/#src/js/script.js
+++ maket_5/#src/js/script.js
@@ -1,5 +1,16 @@
 @@include('alert.js');
+const menuNav = document.getElementById('menu-nav');
+const headerLink = document.querySelectorAll('.header__link');
 
+menuNav.addEventListener('click', event => {
+    event.preventDefault();
+    headerLink.forEach(link => link.classList.remove('active-link'));
+
+    let target = event.target;
+    if (target.classList.contains('header__link')) {
+        target.classList.add('active-link');
+    }
+});
 
 
 
--- maket_5/maket_5/js/script.js
+++ maket_5/maket_5/js/script.js
@@ -1,5 +1,16 @@
-alert('hello');;
+//alert('hello');;
+const menuNav = document.getElementById('menu-nav');
+const headerLink = document.querySelectorAll('.header__link');
 
+menuNav.addEventListener('click', event => {
+    event.preventDefault();
+    headerLink.forEach(link => link.classList.remove('active-link'));
+
+    let target = event.target;
+    if (target.classList.contains('header__link')) {
+        target.classList.add('active-link');
+    }
+});
 
 
 
